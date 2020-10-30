15e68252871b95c62c8b3ea2ee47df773ca2962a
ykurata <yasukokurata@gmail.com>
2020-10-29 23:50:54 -0400

    specified question's type

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,12 @@
 
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answer: string[];
+  question: string;
+  type: string;
+}
 
 export enum Difficulty {
   EASY = "easy",
