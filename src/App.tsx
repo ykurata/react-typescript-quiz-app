df4d62fca800c81fdc69ddedf2ba283912e9b895
ykurata <yasukokurata@gmail.com>
2020-10-29 22:17:24 -0400

    imported fetchdata function and difficulty from api to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-
+import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from "./components/QuestionCard";
+// Types
+import { Difficulty } from './API';
 
 const TOTAL_QUESTIONS = 10;
 
@@ -12,6 +14,8 @@ const App = () => {
   const [useAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
 
   const startTrivia = async () => {
 
@@ -31,14 +35,14 @@ const App = () => {
       <button className="start" onClick={startTrivia}>Start </button>
       <p className="score">Score: </p>
       <p>Loading Qustions...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
