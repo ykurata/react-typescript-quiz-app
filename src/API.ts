c1fe0c586d26d961134107d4aee5bc803dff525a
ykurata <yasukokurata@gmail.com>
2020-10-30 23:54:00 -0400

    added shuffleArray method on utils

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,5 @@
+import QuestionCard from './components/QuestionCard';
+import { shuffleArray } from './utils';
 
 export type Question = {
   category: string;
@@ -19,5 +21,13 @@ export type QuestionState = Question & { answer: string[] };
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results.map((question: Question) => (
+    {
+      ...question,
+      answer: shuffleArray([
+        ...question.incorrect_answer,
+        question.correct_answer
+      ]),
+    }
+  ));
 }
